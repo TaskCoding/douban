bd2cbad573e41c6bb68c8ae62a4992c461aa165c
keith <keith.huang@walkthechat.com>
2019-11-12 20:40:08 +0800

    initialize environment for cloud development

--- app.js
+++ app.js
@@ -48,7 +48,16 @@ App({
    * 生命周期函数--监听小程序初始化
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch () {
+  onLaunch() {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'default',
+        traceUser: false,
+      });
+    }
+
     wechat
       .getLocation()
       .then(res => {
