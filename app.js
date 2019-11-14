e317e41446ddb63022b68990f06dc9e875be8c02
Hanxu2018 <1076998404@qq.com>
2019-11-14 16:44:04 +0800

    JS: initializes cloud development #1
    JS: 初始化云开发 #1

--- app.js
+++ app.js
@@ -49,6 +49,15 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'default',
+        traceUser: false,
+      });
+    }
+
     wechat
       .getLocation()
       .then(res => {
