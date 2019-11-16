0423ea71e1688cc4a6efe3dd4366cda05f78f350
keith <keith.huang@walkthechat.com>
2019-11-16 11:05:11 +0800

    remove the 'default' options in initialization

--- app.js
+++ app.js
@@ -52,10 +52,7 @@ App({
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
-      wx.cloud.init({
-        env: 'default',
-        traceUser: false,
-      });
+      wx.cloud.init({});
     }
 
     wechat
