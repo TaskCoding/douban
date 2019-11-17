c0a8ce2e04bcb60c950568b2d4bdbbd0d678d315
Zira <wasfzxt@foxmail.com>
2019-11-17 16:59:54 +0800

    refresh pull request

--- mini/app.js
+++ mini/app.js
@@ -51,12 +51,10 @@ App({
   onLaunch () {
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({})
     }
-    else {
-      wx.cloud.init({
-        traceUser: true,
-      })
-    }
+
     wechat
       .getLocation()
       .then(res => {
